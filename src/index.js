b5c65bebd08de28c1192abe21593367c00aaec8e
titosouvik <titosouvik@gmail.com>
2022-10-12 12:15:47 +0530

    Added stylesheet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+/** 
 const page = (
     <div>
         <img src="react-logo.png" width = "40px"/>
@@ -14,6 +15,56 @@ const page = (
         </ul>
     </div>
 )
+*/
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(page, document.getElementById("root"))
+//Child Components
+function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img src="react-logo.png" width="40px" />
+        <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function MainContent() {
+  return (
+    <div>
+      <h1>Fun Facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+}
+
+function Footer() {
+  <footer>
+    <small>© 2022 titosouvik development. All rights reserved.</small>
+  </footer>;
+}
+
+// Parent Component
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Page />);
